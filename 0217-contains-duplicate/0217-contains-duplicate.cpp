46d5df0d293faf28b35231e0104c496e41749edf
Swati07-yadav <swatiyadavs866@gmail.com>
2026-09-14 01:26:06 +0530

    Time: 77 ms (33.46%), Space: 113.3 MB (20.33%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        for(int num : nums){
+            mp[num]++;
+        }
+        for(auto it : mp){
+            if(it.second>=2) return true;
+        }
+        return false;
+    }
+};
