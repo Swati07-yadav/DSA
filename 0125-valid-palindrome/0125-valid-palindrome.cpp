4e5055b4eb48d6044be8b43188807f93a9fa6c72
Swati07-yadav <swatiyadavs866@gmail.com>
2026-09-16 19:44:02 +0530

    Time: 3 ms (27.84%), Space: 9.9 MB (77%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int n=s.length();
-        int l=0, r=n-1;
+        int n = s.length();
+        int l = 0 , r= n-1;
         while(l<r){
             if(!isalnum(s[l])){
                 l++;
-            } 
+            }
             else if(!isalnum(s[r])){
                 r--;
             }
-            else if(tolower(s[l])!=tolower(s[r])){
-                 return false;
+            else if(tolower(s[l]) != tolower(s[r])){
+                return false;
             }
             else{
                 l++;
