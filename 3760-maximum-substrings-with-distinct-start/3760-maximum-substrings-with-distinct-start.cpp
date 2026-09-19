479e26bd1a576f9849649216382a50b53e8bf544
Swati07-yadav <swatiyadavs866@gmail.com>
2026-09-19 22:29:07 +0530

    Time: 28 ms (51.2%), Space: 25.4 MB (24.94%) - LeetHub

---
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxDistinct(string s) {
+        int n = s.size();
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++){
+            mp[s[i]]++;
+        }
+        return mp.size();
+    }
+};
