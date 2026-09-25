7d7973e6ad79d68db1ff0c9aa4564649db05ca38
Swati07-yadav <swatiyadavs866@gmail.com>
2026-09-25 23:37:37 +0530

    Time: 0 ms (100%), Space: 8 MB (8.03%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int direction=1;
+        int pos=1;
+        while(time--){
+            pos+=direction;
+            if(pos==n || pos==1){
+                direction *= -1;
+            }
+        }
+        return pos;
+    }
+};
